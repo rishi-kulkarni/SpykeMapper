0ab42c434ae2079de6712ec3d80c451b12cd63d7
rishik111 <insta111@gmail.com>
2017-06-20 22:56:57 -0700

    Add files via upload

--- ROIextraction.py
+++ ROIextraction.py
@@ -9,6 +9,7 @@ from roipoly import roipoly
 import pylab as pl
 from skimage import io
 import numpy as np
+import pandas as pd
 
 plt.ioff() 
     
@@ -16,19 +17,26 @@ plt.ioff()
 image = io.imread("isoTMR.tiff")
 plt.imshow(image[1,:,:])
 
-plt.title("left click: line segment         right click: close region")
-ROI = roipoly(roicolor='r')
+plt.title("Foreground: left click: line segment         right click: close region")
+fgROI = roipoly(roicolor='r')
 plt.show(block=True)
+plt.ioff() 
 
 plt.imshow(image[1,:,:], interpolation='nearest', cmap="Greys")
+plt.title("Background: left click: line segment         right click: close region")
 plt.colorbar()
-ROI.displayROI()
+fgROI.displayROI()
+bgROI = roipoly(roicolor='b')
 
-mask = ROI.getMask(image[1,:,:])
-output = np.zeros((len(image[:,1,1]),2))
+fgmask = fgROI.getMask(image[1,:,:])
+bgmask = bgROI.getMask(image[1,:,:])
+output = np.zeros((len(image[:,1,1]),3))
 timepoints = np.arange(len(image[:,1,1]))
 output[:,0] = timepoints
 for key in range(len(output[:,1])):
-    output[key][1] = pl.mean(image[key,mask])
-
-plt.show(block=True)
+    output[key][1] = pl.mean(image[key,fgmask])
+    output[key][2] = pl.mean(image[key,bgmask])
+ 
+forexcel = pd.DataFrame(output[:,[1,2]], index = output[:,0], columns=["cell","bg"])
+forexcel.index.name = "time"
+forexcel.to_clipboard(excel=True)
