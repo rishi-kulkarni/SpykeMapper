d1ec1d38276cf526aea2f6c9609d6e9f65028bb2
rishik111 <insta111@gmail.com>
2019-09-12 12:00:56 -0700

    Add files via upload

---
+++ spontactanalysis.py
@@ -0,0 +1,116 @@
+"""
+Script for importing an XLSX worksheet of fluorescence traces and creating a new workbook with the following format:
+
+Worksheet 1: Raw Data
+Worksheet 2: Baseline, which contains slow depolarization info
+Worksheet 3: Baseline-corrected spiking data
+
+Created by Rishi Kulkarni
+"""
+
+import voltagestepfunctions as VI
+import numpy as np
+import pandas as pd
+import os
+import easygui
+import itertools
+
+def workbooktoDF():
+    #Find the path to a .xlsx via a GUI fileopen box 
+    print("What XLSX workbook do you want to open?")
+    global filename
+    filename = ""
+    while not filename.endswith(".xlsx"):
+        filename = easygui.fileopenbox(filetypes=["*.xlsx"])        
+    #print(filename)
+    df = pd.read_excel(filename, sheet_name=None)
+    return df
+
+def spike_triggered_baseline(normalized_baseline, spiketrain):
+    #this function takes a continuous baseline function and a digitized spike train and outputs a matrix consisting of the time points +/- t on the baseline from each of the spikes in the spike train. It normalizes the added values to be dF/F values.
+    t = 150 #this is the number of frames away from the spike the function will average
+    spike_trigger_baseline = np.zeros((2*t,1))
+    spike_trigger_baseline[:,0] = np.arange(-t,t)
+    markers = np.asarray(np.where(spiketrain == 1))
+    markers = markers[:,markers[0]>t]
+    markers = markers[:,markers[0]<(np.ma.size(spiketrain[:,0])-t)]
+    for cellnum in range(1, np.max(markers[1])+1):
+        indices = np.where(markers[1] == cellnum)
+        for spiketime in markers[:,indices[0]][0]:
+            toadd = (normalized_baseline[(spiketime-t):(spiketime+t),cellnum])/np.mean(normalized_baseline[:,cellnum])
+            spike_trigger_baseline = np.hstack((spike_trigger_baseline, toadd.reshape(-1,1)))
+    return spike_trigger_baseline
+            
+def spike_triggered_average(sta_matrix):
+    sta_average = np.zeros((300,2))
+    sta_stdev = np.zeros((300,2))
+    sta_average[:,0] = sta_matrix[:,0]
+    sta_stdev[:,0] = sta_matrix[:,0]
+    for i in range(np.ma.size(sta_matrix[:,0])):
+        sta_average[i,1] = np.mean(sta_matrix[i,1:])
+    for i in range(np.ma.size(sta_matrix[:,0])):
+        sta_stdev[i,1] = np.std(sta_matrix[i,1:])
+    return sta_average, sta_stdev
+
+def pairwise_cc(baseline):
+    baselineDF = pd.DataFrame(data=baseline)
+    cross_corr_matrix = np.zeros((len(baselineDF.columns[1:]),len(baselineDF.columns[1:])))
+    cross_corr_lag = np.zeros((len(baselineDF.columns[1:]),len(baselineDF.columns[1:])))
+    for cell1, cell2 in itertools.combinations(baselineDF.columns[1:], 2):
+        cell1base = baselineDF[cell1].values
+        cell2base = baselineDF[cell2].values
+        time = len(cell1base)
+        cell1base = (cell1base - np.mean(cell1base)) / (np.std(cell1base) * len(cell1base))
+        cell2base = (cell2base - np.mean(cell2base)) / (np.std(cell2base))
+        fullcorr = np.correlate(cell1base, cell2base,'full')[time-50:time+50]
+        corr_value = np.max(fullcorr)
+        corr_lag = np.argmax(fullcorr) - 51
+        cross_corr_matrix[cell1-1,cell2-1] = corr_value
+        cross_corr_lag[cell1-1,cell2-1] = corr_lag
+    return cross_corr_matrix, cross_corr_lag
+
+rawDF = workbooktoDF()
+for key in rawDF:
+    folder, outputfile = os.path.split(filename)
+    rawdata = rawDF[key].values
+    baseline = np.copy(rawdata)
+    spikes = np.copy(rawdata)
+
+    for i in range(1,np.ma.size(baseline,1)):
+        actionpotentials = VI.spikesHighPass(spikes[:,i])
+        fitted = VI.baseline_als(baseline[:,i], 10000,0.01)
+        baseline[:,i] = fitted
+        spikes[:,i] = spikes[:,i]/fitted
+
+    dSpikes = np.copy(spikes)
+    dSpikes = VI.digitizeSpikes(dSpikes)
+    BaseNormal = np.copy(baseline)
+    BaseNormal = VI.deltaF(BaseNormal)
+    RasterSpikes = np.copy(spikes)
+    RasterSpikes = VI.RasterSpikes(RasterSpikes)
+    if key != "Concatenated":
+        cross_corr_matrix, cross_corr_lag = pairwise_cc(baseline)
+    
+    baselineDF = pd.DataFrame(data=baseline, index = rawdata[:,0], columns = rawDF[key].columns)
+    BaseNormalDF = pd.DataFrame(data=BaseNormal, index = rawdata[:,0], columns = rawDF[key].columns)
+    spikesDF = pd.DataFrame(data=spikes, index = rawdata[:,0], columns = rawDF[key].columns)
+    dSpikesDF = pd.DataFrame(data=dSpikes, index = rawdata[:,0], columns = rawDF[key].columns)
+    if key != "Concatenated":
+        cross_corr_matrix = pd.DataFrame(data=cross_corr_matrix, index = rawDF[key].columns[1:], columns = rawDF[key].columns[1:])
+        cross_corr_lag = pd.DataFrame(data=cross_corr_lag, index = rawDF[key].columns[1:], columns = rawDF[key].columns[1:])
+    
+    if not os.path.exists(folder+"\\"+os.path.splitext(outputfile)[0]):
+        os.mkdir(folder+"\\"+os.path.splitext(outputfile)[0])
+
+    with pd.ExcelWriter(folder+"\\"+os.path.splitext(outputfile)[0]+"\\"+key+".xlsx") as writer:
+        rawDF[key].to_excel(writer, sheet_name="Raw Data")
+        baselineDF.to_excel(writer, sheet_name="Baseline_Filtered")
+        BaseNormalDF.to_excel(writer, sheet_name="Baseline_Normalized")
+        spikesDF.to_excel(writer, sheet_name="Spikes_Filtered")
+        dSpikesDF.to_excel(writer,sheet_name="Spike Train")
+        if key != "Concatenated":
+            cross_corr_matrix.to_excel(writer,sheet_name="Baseline_CC_R_Values")
+            cross_corr_lag.to_excel(writer,sheet_name="Baseline_CC_Time_Lags")
+
+
+print("Done!")
--- voltagestepfunctions.py
+++ voltagestepfunctions.py
@@ -6,7 +6,6 @@ Created on Tue Jun 13 16:24:12 2017
 
 @author: Rishi
 """
-
 import easygui
 import numpy as np
 import pandas as pd
@@ -15,6 +14,10 @@ import scipy.sparse as sparse
 from peakdet import peakdet
 import sys
 from numpy import NaN, Inf, arange, isscalar, asarray, array
+from scipy.signal import butter, lfilter
+from scipy.signal import freqs
+import scipy.signal as signal
+import matplotlib.pyplot as plt
 
 def workbooktoarray():
     #Find the path to a .xlsx via a GUI fileopen box
@@ -35,12 +38,69 @@ def workbooktoarray():
     #Pick what columns to bring into the numpy array (A:C or A,B,C works fine)
     print("Which columns contain data? Input letters separated with commas.")
     parsecol = input()
-    df = pd.read_excel(filename, sheetname=sheetnum,header=0, parse_cols=parsecol)
-    df = df.values
+    df = pd.read_excel(filename, sheet_name=sheetnum,header=0, usecols=parsecol)
     return df
 
+def digitizeSpikes(spikearray):
+    #this function will convert the filtered spikes to a 0-1 spike train, effectively digitizing the data
+    digitize_spikes = np.zeros_like(spikearray) #this makes an array of 0s that will look like the input array
+    digitize_spikes[:,0] = spikearray[:,0] #this sets the first column equal to time indices
+    for cellnum in range(1,np.ma.size(spikearray,1)):
+        threshold = 4.0*np.average(np.std(spikearray[:,1:],axis=0)) + np.median(spikearray[:,cellnum])
+        above_thresh = np.where(spikearray[:,cellnum] > threshold)
+        maxima = signal.argrelextrema(spikearray[:,cellnum], np.greater, order=2)
+        spike_index = np.intersect1d(maxima, above_thresh)
+        for index in spike_index:
+            digitize_spikes[index,cellnum] = 1
+    return digitize_spikes
 
 
+def RasterSpikes(spikearray):
+    #this function will convert the filtered spikes to a NaN-1 spike train, enabling easy plotting as a raster plot. If you plot this in matplotlib and place a line marker "|", you will get a traditional looking raster plot
+    for i in range(1,np.ma.size(spikearray,1)):
+        threshold = np.std(spikearray[:,i])
+        rearm = 0
+        for j in range(0,np.ma.size(spikearray,0)):
+            if (spikearray[j,i] > 4.5*threshold):
+                if (rearm == 0):
+                    spikearray[j,i] = 1
+                    rearm = 1
+                else:
+                    spikearray[j,i] = np.nan
+            else:
+                spikearray[j,i] = np.nan
+                rearm = 0
+    return spikearray
+
+def plotRaster(RasterSpikes):
+    #RasterSpikes, in this case, refers to an array that was produced via running the RasterSpikes() function on a filtered dataset containing noise and spikes. This function sets the x and y axes based on the size of the RasterSpikes array
+    toplot = RasterSpikes[RasterSpikes > 0.1]
+    for i in range(1,np.ma.size(RasterSpikes,1)):
+        plt.plot(RasterSpikes[:,0], toplot[:,i] + i, label='baseline', marker='|', markersize=12)
+    plt.xlim(0,np.ma.size(RasterSpikes[:,1]))
+    plt.ylim(0,np.ma.size(RasterSpikes[1,:]) + 1)
+
+def plotBaselineRaster(dSpikes, BaseNormal):
+    #This function graphs the baseline values of all the cells in the BaseNormal array with an offset and then marks all the points that have an action potential with an upward line marker, effectively generating a raster plot that shows what the relative resting membrane potential was prior to that spike. The resting membrane potential is normalized to some value between 0 and 1 for ease of visualization. 
+    for i in range(1,np.ma.size(BaseNormal,1)):
+        markers = np.where(dSpikes[:,i] == 1)
+        markers = np.asarray(markers)
+        markers = markers.tolist()
+        plt.plot(BaseNormal[:,0], i + ((BaseNormal[:,i]-np.min(BaseNormal[:,i]))/(np.max(BaseNormal[:,i]) - np.min(BaseNormal[:,i]))), label='baseline', markevery=markers, marker=2, markersize=12)
+    plt.xlim(0,np.ma.size(BaseNormal[:,1]))
+    
+def plotBaseline(BaseNormal):
+    #This function graphs the baseline values of all the cells in the BaseNormal array with an offset. The resting membrane potential is normalized to some value between 0 and 1 for ease of visualization.
+    for i in range(1,np.ma.size(BaseNormal,1)):
+        plt.plot(BaseNormal[:,0], i + ((BaseNormal[:,i]-np.min(BaseNormal[:,i]))/(np.max(BaseNormal[:,i]) - np.min(BaseNormal[:,i]))), label='baseline',)
+    plt.xlim(0,np.ma.size(BaseNormal[:,1]))
+    
+def deltaF(baseline):
+    for i in range(1,np.ma.size(baseline,1)):
+        MaxFluor = np.mean(baseline[:,i])
+        baseline[:,i] = np.divide(baseline[:,i],MaxFluor)
+    return baseline
+    
 def backgroundSubtract(fluorarray):
     #generate a linear regression of the background, which is assumed to be column 3 of the array. column 1 is assumed to be timepoints.
     bgarray = fluorarray[:,[0,2]]
@@ -55,8 +115,8 @@ def backgroundSubtract(fluorarray):
     fluorarray[:,1] = sub
     return fluorarray
 
-def baseline_als(y, lam, p, niter=20):
-    #performs a asymmetric least squares regression to figure out the baseline
+def baseline_als(y, lam, p, niter=10):
+    #performs a asymmetric least squares regression to figure out the baselinebas
     L = len(y)
     D = sparse.csc_matrix(np.diff(np.eye(L), 2))
     w = np.ones(L)
@@ -67,12 +127,32 @@ def baseline_als(y, lam, p, niter=20):
         w = p * (y > z) + (1-p) * (y < z)
     return z
 
+def baselineLowPass(rawdata):
+    fs = 500  # Sampling frequency
+    t = np.arange(2000) / fs
+    rawx = rawdata
+    fc = 3  # Cut-off frequency of the filter
+    w = fc / (fs / 2) # Normalize the frequency
+    b, a = signal.butter(5, w, 'low')
+    output = signal.filtfilt(b, a, rawx)
+    return output
+
+def spikesHighPass(rawdata):
+    fs = 500  # Sampling frequency
+    t = np.arange(2000) / fs
+    rawx = rawdata
+    fc = 3  # Cut-off frequency of the filter
+    w = fc / (fs / 2) # Normalize the frequency
+    b, a = signal.butter(5, w, 'high')
+    output = signal.filtfilt(b, a, rawx)
+    return output
+
 def baselineFlatten(bgcorrected):
     #divides out the baseline to produce dF/F plot. 
     ydata = bgcorrected[:,1]
     #you can change the fitting parameters for better fitting. 
-    fitted = baseline_als(ydata, 100000, 0.001)
-    fitted = baseline_als(fitted, 100000, 0.99999999)
+    fitted = baseline_als(ydata, 10000, 0.0001)
+#    fitted = baseline_als(fitted, 1000000, 1, niter=17)
     deltaf = np.divide(bgcorrected[:,1],fitted)
     bgcorrected[:,1] = deltaf
     return bgcorrected
@@ -176,14 +256,14 @@ def smooth(x,window_len=11,window='hanning'):
 
 
     
-if __name__=="__main__":
-    from matplotlib.pyplot import plot, scatter, show
-    rawdata = workbooktoarray()
-    bgsub = backgroundSubtract(rawdata)
-    bleachcorr = baselineFlatten(bgsub)
-    dff = stepFinder(bleachcorr)
-    plot(dff[:,0],dff[:,1])
-    forexcel = pd.DataFrame(dff[:,1], index = dff[:,0], columns=["dF/F"])
-    forexcel.index.name = "mV"
-    forexcel.to_clipboard(excel=True)
-    print("Voltage Sensitivity Data copied to clipboard")
+#if __name__=="__main__":
+#    from matplotlib.pyplot import plot, scatter, show
+#    rawdata = workbooktoarray()
+#    bgsub = backgroundSubtract(rawdata)
+#    bleachcorr = baselineFlatten(bgsub)
+#    dff = stepFinder(bleachcorr)
+#    plot(dff[:,0],dff[:,1])
+#    forexcel = pd.DataFrame(dff[:,1], index = dff[:,0], columns=["dF/F"])
+#    forexcel.index.name = "mV"
+#    forexcel.to_clipboard(excel=True)
+#    print("Voltage Sensitivity Data copied to clipboard")
